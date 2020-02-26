9de24a9c0394ed06e8e251507c10f39f410c15c3
Brent Schmidt <bschmidt@oreilly.com>
2020-02-26 12:06:21 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+const log = require('fancy-log');
+const puppeteer = require('puppeteer');
+
+const sleep = duration => new Promise(resolve => setTimeout(resolve, duration));
+
+const prod = 'https://www.oreilly.com/member/login';
+const prodGcp = 'https://grootfrontend.platform-prod.gcp.oreilly.com/member/login';
+
+const currentUrl = prod;
+const maxIterations = 10;
+
+(async () => {
+  const errors = [];
+  const browser = await puppeteer.launch({ headless: false });
+
+  const page = await browser.newPage();
+  await page.setCacheEnabled(false);
+
+  log.info('Initial page load.', currentUrl);
+  await page.goto(currentUrl);
+
+  // page.on('error', err => {
+  //   console.log('ERROR', err);
+  // });
+
+  // page.on('pageerror', pageerr => {
+  //   console.log('PAGEERROR', pageerr);
+  // })
+
+  page.on('requestfailed', request => {
+    const url = new URL(request.url(), currentUrl);
+    if (url.host.includes('oreilly.com')) {
+      const response = request.response();
+      const failure = request.failure();
+      let statusCode;
+      if (response) {
+        statusCode = response.status();
+      }
+      errors.push({
+        url: request.url(),
+        statusCode,
+        errorText: failure.errorText
+      })
+    }
+  });
+
+  let i = 0;
+  while (++i < maxIterations) {
+    try {
+      await page.waitForSelector('h1', { timeout: 15000 });
+      log.info('Page reload:', i);
+      await page.reload({ waitUntil: 'networkidle0' });
+      await sleep(2000);
+    } catch (e) {
+      errors.push({
+        url: currentUrl,
+        statusCode: 0,
+        errorText: e.message
+      });
+    }
+  }
+
+  log.info('Error Count:', errors.length);
+  errors.forEach(e => log.info('Error:', e.url, e.statusCode, e.errorText));
+
+  await browser.close();
+})();
